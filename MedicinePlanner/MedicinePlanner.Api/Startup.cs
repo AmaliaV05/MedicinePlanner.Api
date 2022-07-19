7a0c1df4dd6d6df8f1dc63079ae01d06245366e0
Amalia Vargatu <avargatu@yahoo.com>
2022-07-19 14:33:10 +0300

    add business logic and data layers; add database connection

--- MedicinePlanner/MedicinePlanner.Api/Startup.cs
+++ MedicinePlanner/MedicinePlanner.Api/Startup.cs
@@ -1,5 +1,7 @@
+using MedicinePlanner.Data.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +20,9 @@ namespace MedicinePlanner.Api
         
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")) );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
